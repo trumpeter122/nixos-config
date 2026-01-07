76974949ce6b547f08e0ab1a604a15503b83cd0c
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-01-07 12:22:08 +0800

    auto: 2026-01-07-12:22

--- nixos/desktop/pps.nix
+++ nixos/desktop/pps.nix
@@ -10,6 +10,6 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.gnome.enable = true;
   services.desktopManager.cosmic.enable = true;
-  services.system76-scheduler.enable = true;
+  # services.system76-scheduler.enable = true;
 
 }
