77867a793e418fc271ba8bb1a05652ff3e2698b1
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-01-07 14:39:31 +0800

    auto: 2026-01-07-14:39

--- nixos/pps.nix
+++ nixos/pps.nix
@@ -11,4 +11,10 @@
   programs.steam.enable = true;
 
   services.v2raya.enable = true;
+
+  environment.etc."udev/rules.d/59-vial.rules" = {
+    text = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    '';
+  };
 }
