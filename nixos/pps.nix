af184bc9619b4de1b5a7a9c997e944f09eba7f5d
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-01-07 14:34:36 +0800

    auto: 2026-01-07-14:34

--- nixos/pps.nix
+++ nixos/pps.nix
@@ -11,7 +11,4 @@
   programs.steam.enable = true;
 
   services.v2raya.enable = true;
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-  '';
 }
