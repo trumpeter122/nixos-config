ac70a62b6c0163c9b594f044f0c9a7fec72a76b7
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2026-01-16 11:24:48 +0800

    auto: 2026-01-16-11:24

--- flake.nix
+++ flake.nix
@@ -12,9 +12,9 @@
 
     # Home manager
     # home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.url = "git+https://gitee.com/mirrors/home-manager-nix/blob/release-25.11";
+    # home-manager.url = "git+https://gitee.com/mirrors/home-manager-nix/blob/release-25.11";
     # home-manager.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nix-community/home-manager.git?ref=release-25.11&shallow=1";
-    # home-manager.url = "git+https://git.nju.edu.cn/nix-community/home-manager.git";
+    home-manager.url = "git+https://git.nju.edu.cn/nix-community/home-manager.git";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Extras
